69fadce36d77fead919588d98b037a0383b4b2cf
LucasEncenha <lucasalvesencenha@gmail.com>
2025-06-27 02:22:30 -0300

    Adiciona rota de teste para deploy

--- backend/projeto_ferreira_mel/routes/web.php
+++ backend/projeto_ferreira_mel/routes/web.php
@@ -14,19 +14,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->middleware(['auth']);
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::get('/paginaLink', function (Request $request) {
-        $token = $request->query('token');
-        return view('paginaLink', compact('token'));
-    })->name('paginaLink');
+Route::get('/teste-render', function () {
+    return 'Laravel está funcionando no Render!';
 });
 
-require __DIR__.'/auth.php';
+// Route::get('/', function () {
+//     return view('welcome');
+// })->middleware(['auth']);
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+//     Route::get('/paginaLink', function (Request $request) {
+//         $token = $request->query('token');
+//         return view('paginaLink', compact('token'));
+//     })->name('paginaLink');
+// });
+
+// require __DIR__.'/auth.php';
